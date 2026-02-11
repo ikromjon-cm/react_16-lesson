c4563232f02319d8c7003ca1a0ce07ea14cc9c2b
user <akparaliyevikromjon7@gmail.com>
2026-02-11 15:29:51 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import React from 'react';
+// get
+axios
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data.data))
+  .catch(err => console.error(err));
+
 
 const App = () => {
   return (
     <div>
-      
+
     </div>
   );
 }
