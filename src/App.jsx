3f6e2cf9f042e2ae9d5cf56596be54b200eaaa1b
user <akparaliyevikromjon7@gmail.com>
2026-02-11 15:35:27 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ axios
   .get('https://jsonplaceholder.typicode.com/users')
   .then(data => console.log(data.data))
   .catch(err => console.error(err));
+// post
+axios
+.post('http://jsonplaceholder.typicode.com/posts',{
+  title:"pumpkins",
+  body:"salom",
+  userId:1
+})
+.then(res=>console.log(res.data))
+.catch(err => console.error(err));
+
 
 
 const App = () => {
